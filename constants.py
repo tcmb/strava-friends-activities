79df2037a7cadb72518991962f911404602d3405
Florian Ilgenfritz <florian@newstore.com>
2017-04-09 18:45:58 +0200

    removing obsolete client secret

--- constants.py
+++ constants.py
@@ -3,8 +3,6 @@ from stravalib.attributes import LatLon
 
 ACCESS_TOKEN = "MY_ACCESS_TOKEN"
 
-CLIENT_SECRET = "MY_CLIENT_SECRET"
-
 P = 0.017453292519943295
 
 HOME_LOC = LatLon(lat=52.47, lon=13.45)
